68021b736ba6371b9fb185da76f09a7a23ff2727
Sachin Saini <iamsainisachin@gmail.com>
2018-10-27 10:34:43 +0530

    final commmit

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const fileUpload = require('express-fileupload');
 const crypto = require('crypto');
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(fileUpload());
+
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
 app.post('/upload', (req, res) => {
 	const hash = crypto.createHash('sha256').update(req.files.foo.data).digest('hex');
 	res.json(hash);
 });
-app.listen(process.env.PORT, () => {
+
+app.listen(8080, () => {
 	console.log('server running');
 });
